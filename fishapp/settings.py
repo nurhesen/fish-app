3293da053bf54f74e04f5125409f98804ea13452
Nuru Hasanov <ciyeleklilollipop@gmail.com>
2022-07-17 23:53:48 +0400

    debug

--- fishapp/settings.py
+++ fishapp/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"frontend","build","static"),
  #   '/var/www/static/',
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'out')
 
 
 # Default primary key field type
